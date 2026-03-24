3d17cbf4f9345eded106ef2730a995f7bee35d55
Spanos Panos <panosqwe632@gmail.com>
2026-03-24 15:06:30 +0200

    bugs fixed

--- ReactBitsComponents/Backgrounds/Dither/Dither.tsx
+++ ReactBitsComponents/Backgrounds/Dither/Dither.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 import { useRef, useEffect, forwardRef } from "react";
-import { Canvas, useFrame, useThree, ThreeEvent } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import type { ThreeEvent } from "@react-three/fiber";
 import { EffectComposer, wrapEffect } from "@react-three/postprocessing";
 import { Effect } from "postprocessing";
 import * as THREE from "three";
@@ -158,12 +159,13 @@ class RetroEffectImpl extends Effect {
   }
 }
 
+const WrappedRetroEffect = wrapEffect(RetroEffectImpl);
+
 const RetroEffect = forwardRef<
   RetroEffectImpl,
   { colorNum: number; pixelSize: number }
 >((props, ref) => {
   const { colorNum, pixelSize } = props;
-  const WrappedRetroEffect = wrapEffect(RetroEffectImpl);
   return (
     <WrappedRetroEffect ref={ref} colorNum={colorNum} pixelSize={pixelSize} />
   );
